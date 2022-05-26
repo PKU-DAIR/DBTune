4003b267c5bc2a778f176dc9064db96ea73df4aa
Blairruc-pku <zhang_xinyi@pku.edu.cn>
2022-05-26 11:15:47 +0800

    support more congfig.ini file

--- autotune/utils/config.py
+++ autotune/utils/config.py
@@ -2,7 +2,7 @@
 
 import configparser
 import json
-
+from collections import defaultdict
 
 class DictParser(configparser.ConfigParser):
     def read_dict(self):
@@ -12,28 +12,24 @@ class DictParser(configparser.ConfigParser):
         return d
 
 
-cf = DictParser()
-cf.read("config.ini", encoding="utf-8")
-config_dict = cf.read_dict()
+
+knob_config = {}
+
+def get_default_dict(dic):
+    default_dic =  defaultdict(str)
+    for k in dic:
+        default_dic[k] = dic[k]
+
+    return default_dic
+
+def parse_args(file):
+    cf = DictParser()
+    cf.read(file, encoding="utf-8")
+    config_dict = cf.read_dict()
+    global knob_config
+    f = open(config_dict['database']['knob_config_file'])
+    knob_config = json.load(f)
+
+    return get_default_dict(config_dict["database"]), get_default_dict(config_dict['tune'])
 
 
-def parse_args():
-    return config_dict["database"], config_dict['tune']
-
-
-def parse_knob_config():
-    f =  open(config_dict['database']['knob_config_file'])
-    _knob_config =  json.load(f)
-    #for key in _knob_config:
-    #    _knob_config[key] = json.loads(str(_knob_config[key]).replace("\'", "\""))
-    return _knob_config
-
-
-
-knob_config = parse_knob_config()
-# sync with main.py
-
-
-#predictor_output_dim = int(config_dict["predictor"]["predictor_output_dim"])
-
-#predictor_epoch = int(config_dict["predictor"]["predictor_epoch"])
--- scripts/optimize.py
+++ scripts/optimize.py
@@ -3,10 +3,15 @@ from autotune.database.mysqldb import MysqlDB
 from autotune.database.postgresqldb import PostgresqlDB
 from autotune.dbenv import DBEnv
 from autotune.tuner import DBTuner
-
+import argparse
 
 if __name__ == '__main__':
-    args_db, args_tune = parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='config.ini', help='config file')
+    opt = parser.parse_args()
+
+
+    args_db, args_tune = parse_args(opt.config)
 
     if args_db['db'] == 'mysql':
         db = MysqlDB(args_db)
