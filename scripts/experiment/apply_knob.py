015a1d2ecb55531d4c13d63b67c3b1378ac25632
Blairruc-pku <zhang_xinyi@pku.edu.cn>
2022-05-28 17:21:22 +0800

    more scripts

--- scripts/experiment/apply_knob.py
+++ scripts/experiment/apply_knob.py
@@ -1,19 +1,44 @@
 import json
 import sys
+from autotune.utils.config import parse_args
+from autotune.database.mysqldb import MysqlDB
+from autotune.database.postgresqldb import PostgresqlDB
+from autotune.dbenv import DBEnv
+from autotune.tuner import DBTuner
+import argparse
+
+def parse(run_history, apply_option):
+    f = open(run_history)
+    res = json.load(f)['data']
+    if apply_option == 'default':
+        config = res[0]["configuration"]
+    else:
+        config, best_tps = res[0], 0
+        for c in res:
+            if "tps"  in c["external_metrics"].keys() and  c["external_metrics"]["tps"] > best_tps:
+                config = c["configuration"]
+                best_tps = c["external_metrics"]["tps"]
+    return config
 
 if __name__ == '__main__':
-    f_json = sys.argv[1]
-    output_file = f_json.split('.')[0] + '_dynamic.json'
-    f_json = open(f_json)
-    konb_template = json.load(f_json)
-    out_knob = {}
-    for key in konb_template.keys():
-        if konb_template[key]['dynamic'] == "Yes":
-            out_knob[key] = konb_template[key]
+    run_history = sys.argv[1]
+    apply_option = sys.argv[2]
+    config = parse(run_history, apply_option)
+
+    args_db, args_tune = parse_args("/root/demo/DBTuner-main/scripts/config.ini")
+    if args_db['db'] == 'mysql':
+        db = MysqlDB(args_db)
+    elif args_db['db'] == 'postgresql':
+        db = PostgresqlDB(args_db)
+
+    env = DBEnv(args_db, args_tune, db)
+    timeout, metrics, internal_metrics, resource = env.step_GP(config)
+    print(' objective value: %s.' % (metrics[0]))
 
 
-    with open(output_file, 'w') as fp:
-        json.dump(out_knob, fp, indent=4)
+
+
+
 
 
 
